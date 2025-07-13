bf525b159c1d01b34a54ed3d380c2a097676edf8
reflaxess123 <reflaxess@yandex.ru>
2025-07-14 00:00:23 +0300

    _

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -3,4 +3,5 @@ require "nvchad.mappings"
 local map = vim.keymap.set
 
 map("i", "jk", "<ESC>")
+map("t", "<ESC>", "<C-\\><C-n>")
 vim.keymap.set("n", "<leader>pq", ":bd!<CR>", { desc = "Force close buffer", noremap = true, silent = true })
