622d0b3bc031dba6feb62a5fb3ab6e4801f37742
reflaxess123 <reflaxess@yandex.ru>
2025-07-24 17:09:17 +0300

    Фиксы

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -12,6 +12,11 @@ M.base46 = {
   hl_override = {
     Comment = { italic = true },
     ["@comment"] = { italic = true },
+    -- Волнистое подчеркивание для диагностики
+    DiagnosticUnderlineError = { undercurl = true, sp = "red" },
+    DiagnosticUnderlineWarn = { undercurl = true, sp = "orange" },
+    DiagnosticUnderlineInfo = { undercurl = true, sp = "blue" },
+    DiagnosticUnderlineHint = { undercurl = true, sp = "green" },
   },
 }
 
@@ -30,5 +35,22 @@ M.plugins = {
     enabled = false,
   },
 }
+
+-- Настройка диагностики LSP
+vim.diagnostic.config({
+  virtual_text = false, -- Отключаем inline ошибки
+  underline = true, -- Включаем подчеркивание
+  signs = true,
+  update_in_insert = false,
+  severity_sort = true,
+})
+
+-- Принудительно устанавливаем волнистое подчеркивание
+vim.cmd([[
+  hi DiagnosticUnderlineError gui=undercurl guisp=red cterm=undercurl
+  hi DiagnosticUnderlineWarn gui=undercurl guisp=yellow cterm=undercurl  
+  hi DiagnosticUnderlineInfo gui=undercurl guisp=blue cterm=undercurl
+  hi DiagnosticUnderlineHint gui=undercurl guisp=green cterm=undercurl
+]])
 
 return M
--- lua/mappings.lua
+++ lua/mappings.lua
@@ -11,3 +11,17 @@ map("n", "<C-Down>", "<C-w>j", { desc = "Move to window below", silent = true })
 map("n", "<C-Left>", "<C-w>h", { desc = "Move to window left", silent = true })
 map("n", "<C-Right>", "<C-w>l", { desc = "Move to window right", silent = true })
 map("n", "<leader>e", ":NvimTreeToggle<CR>", { desc = "Toggle nvim tree", silent = true })
+
+-- Диагностика LSP
+map("n", "<leader>dd", vim.diagnostic.open_float, { desc = "Show diagnostic message", silent = true })
+
+-- Переключение inline диагностики
+local function toggle_virtual_text()
+  local config = vim.diagnostic.config()
+  vim.diagnostic.config({
+    virtual_text = not config.virtual_text
+  })
+  print("Virtual text: " .. (config.virtual_text and "disabled" or "enabled"))
+end
+
+map("n", "<leader>dt", toggle_virtual_text, { desc = "Toggle inline diagnostic text", silent = true })
